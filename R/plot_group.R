37a10097f75efbb4e5fd5224769bda016ba446ca
Eduard Bukin <edwardbukin@gmail.com>
2017-01-20 16:42:00 +0100

    Developing improvements for the plot_group function.

--- R/plot_group.R
+++ R/plot_group.R
@@ -1,5 +1,22 @@
-plot_group <- function(df, n_page, groups_var, plots_var, timeseries_var) {
-  if (is.na(groups_var)) groups_var <- "Any"
-  d_ply(df, groups_var, plot_subgroup, n_page, plots_var, timeseries_var,
-      .progress = "text")
+plot_group <- function(df,
+                       n_page = 1,
+                       groups_var = NA,
+                       plots_var = NA,
+                       timeseries_var = "d.source",
+                       all_vars = c("AreaCode", "ItemCode", "ElementCode", "d.source")) {
+  # Everything field for files by
+  df <- df %>% mutate(All = 1)
+  
+  if (any(is.na(groups_var))) groups_var <- "All"
+  
+  d_ply(
+    df,
+    groups_var,
+    plot_subgroup,
+    n_page,
+    plots_var,
+    timeseries_var,
+    all_vars,
+    .progress = "text"
+  )
 }
